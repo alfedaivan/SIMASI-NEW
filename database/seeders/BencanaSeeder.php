77ea8a9f29852123111c8c943a3e200d18d3e748
Muhammad Rizqi Ardiansyah <muh.rizqiardian@gmail.com>
2022-12-16 16:49:47 +0700

    #20ShowTotalPengungsiInSpesificBencana

--- database/seeders/BencanaSeeder.php
+++ database/seeders/BencanaSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Bencana;
+use Carbon\Carbon;
 
 class BencanaSeeder extends Seeder
 {
@@ -15,6 +16,30 @@ class BencanaSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Bencana::factory(10)->create();
+        // \App\Models\Bencana::factory(10)->create();
+        DB::table('bencana')->insert([
+            [
+                'name' => 'Gunung Kelud Meletus',
+                'tanggal' => Carbon::parse('2014-02-13'),
+                'waktu' => '12:34:40',
+                'lokasi' => 'Blitar, Malang, Kediri',
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Agency',
+                'email' => 'agency@app.com',
+                'password' => bcrypt('password'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'End',
+                'email' => 'endcustomer@app.com',
+                'password' => bcrypt('password'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+
+        ]);
     }
 }
